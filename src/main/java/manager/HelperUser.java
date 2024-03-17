74afd5b200120a6e53db75568d86078b68f1e40a
Smolawer <simhawer@gmail.com>
2024-03-17 11:40:21 +0200

    77

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -29,5 +29,9 @@ public class HelperUser extends HelperBase {
     public boolean isLogged() {
         return isElementPresent(By.xpath("//button[text()='Sign Out']"));
     }
+
+    public void logout() {
+        click(By.xpath("//button[text()='Sign Out']"));
+    }
 }
 
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -2,9 +2,16 @@ package tests;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
+    @BeforeMethod
+    public void preCondition(){
+        if(app.getHelperUser().isLogged()){
+            app.getHelperUser().logout();
+        }
+    }
 
     @Test
     public void LoginSuccsess (){
@@ -15,6 +22,14 @@ public class LoginTests extends TestBase{
 
 
     }
+    @Test
+    public void LoginSuccsessModel (){
+        app. getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("mara@gmail.com","Mmar123456$");
+        app.getHelperUser().submitLogin();
+        Assert.assertTrue(app.getHelperUser().isLogged());
 
+
+    }
 
 }
